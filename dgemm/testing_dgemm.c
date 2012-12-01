affec5716e6b4f27085dcf949010930f53c1c9e6
Kazuya Matsumoto <d8121101@u-aizu.ac.jp>
2012-12-01 18:32:01 +0900

    supported dgemm of AMD clBLAS

--- dgemm/testing_dgemm.c
+++ dgemm/testing_dgemm.c
@@ -6,13 +6,19 @@
 #ifdef TESTING_ATLAS
 #include <cblas.h>
 #include <clapack.h>
-#define DGEMM_COL() cblas_dgemm(order, transA, transB, M, N, K, alpha, A, lda, B, ldb, beta, C, ldc)
-#define DGEMM_ROW() cblas_dgemm(order, transA, transB, M, N, K, alpha, A, lda, B, ldb, beta, C, ldc)
+#define DGEMM_COL() cblas_dgemm(Order, TransA, TransB, M, N, K, alpha, A, lda, B, ldb, beta, C, ldc)
+#define DGEMM_ROW() cblas_dgemm(Order, TransA, TransB, M, N, K, alpha, A, lda, B, ldb, beta, C, ldc)
 #elif defined(TESTING_MKL)
 #include <mkl_cblas.h>
 #include <mkl_lapack.h>
-#define DGEMM_COL() cblas_dgemm(order, transA, transB, M, N, K, alpha, A, lda, B, ldb, beta, C, ldc)
-#define DGEMM_ROW() cblas_dgemm(order, transA, transB, M, N, K, alpha, A, lda, B, ldb, beta, C, ldc)
+#define DGEMM_COL() cblas_dgemm(Order, TransA, TransB, M, N, K, alpha, A, lda, B, ldb, beta, C, ldc)
+#define DGEMM_ROW() cblas_dgemm(Order, TransA, TransB, M, N, K, alpha, A, lda, B, ldb, beta, C, ldc)
+#elif defined(TESTING_AMDCLBLAS)
+#include <cblas.h>
+#include <clapack.h>
+#include <clAmdBlas.h>
+#define DGEMM_COL() clAmdBlasDgemm(order, transa, transb, M, N, K, alpha, bufA, lda, bufB, ldb, beta, bufC, ldc, 1, &queue, 0, NULL, &event)
+#define DGEMM_ROW() clAmdBlasDgemm(order, transa, transb, M, N, K, alpha, bufA, lda, bufB, ldb, beta, bufC, ldc, 1, &queue, 0, NULL, &event)
 #endif
 
 #include "flops.h"
@@ -26,37 +32,77 @@ int main(int argc, char *argv[])
   int ISEED[4] = {1,0,0,1};
   double neg_one = D_NEG_ONE;
   double start, end, comp_time, perf, error, work[1];
+  int M, N, K, lda, ldb, ldc, sizeA, sizeB, sizeC;
 
-#ifdef TESTING_ATLAS
-  const enum CBLAS_ORDER order = (argc <= 1) ? CblasColMajor : ((atoi(argv[1])==0) ? CblasColMajor : CblasRowMajor);
-  const enum CBLAS_TRANSPOSE transA = (argc <= 2) ? CblasNoTrans : ((atoi(argv[2])==0) ? CblasNoTrans : CblasTrans);
-  const enum CBLAS_TRANSPOSE transB = (argc <= 3) ? CblasNoTrans : ((atoi(argv[3])==0) ? CblasNoTrans : CblasTrans);
-#else
-  const CBLAS_ORDER order = (argc <= 1) ? CblasColMajor : ((atoi(argv[1])==0) ? CblasColMajor : CblasRowMajor);
-  const CBLAS_TRANSPOSE transA = (argc <= 2) ? CblasNoTrans : ((atoi(argv[2])==0) ? CblasNoTrans : CblasTrans);
-  const CBLAS_TRANSPOSE transB = (argc <= 3) ? CblasNoTrans : ((atoi(argv[3])==0) ? CblasNoTrans : CblasTrans);
+#if defined(TESTING_ATLAS) || defined(TESTING_AMDCLBLAS)
+  const enum CBLAS_ORDER Order = (argc <= 1) ? CblasColMajor : ((atoi(argv[1])==0) ? CblasColMajor : CblasRowMajor);
+  const enum CBLAS_TRANSPOSE TransA = (argc <= 2) ? CblasNoTrans : ((atoi(argv[2])==0) ? CblasNoTrans : CblasTrans);
+  const enum CBLAS_TRANSPOSE TransB = (argc <= 3) ? CblasNoTrans : ((atoi(argv[3])==0) ? CblasNoTrans : CblasTrans);
+#elif defined(TESTING_MKL)
+  const CBLAS_ORDER Order = (argc <= 1) ? CblasColMajor : ((atoi(argv[1])==0) ? CblasColMajor : CblasRowMajor);
+  const CBLAS_TRANSPOSE TransA = (argc <= 2) ? CblasNoTrans : ((atoi(argv[2])==0) ? CblasNoTrans : CblasTrans);
+  const CBLAS_TRANSPOSE TransB = (argc <= 3) ? CblasNoTrans : ((atoi(argv[3])==0) ? CblasNoTrans : CblasTrans);
 #endif
+
   const int max_size = (argc <= 4) ? 32 : atoi(argv[4]);
   const int stride = (argc <= 5) ? 1 : atoi(argv[5]);
   const int error_check = (argc <= 6) ? 0 : atoi(argv[6]);
-
   double *A  = (double *)memalign(256, max_size*max_size*sizeof(double));
   double *B  = (double *)memalign(256, max_size*max_size*sizeof(double));
   double *C  = (double *)memalign(256, max_size*max_size*sizeof(double));
   double *C2 = (double *)memalign(256, max_size*max_size*sizeof(double));
 
+#ifdef TESTING_AMDCLBLAS
+  cl_int err;
+  cl_platform_id platform;
+  cl_device_id device;
+  cl_context_properties props[3] = { CL_CONTEXT_PLATFORM, 0, 0 };
+  cl_context ctx;
+  cl_command_queue queue;
+  cl_mem bufA, bufB, bufC;
+  cl_event event = NULL;
+  const clAmdBlasOrder order = (Order == CblasColMajor) ? clAmdBlasColumnMajor : clAmdBlasRowMajor;
+  const clAmdBlasTranspose transa = (TransA == CblasNoTrans) ? clAmdBlasNoTrans : clAmdBlasTrans;
+  const clAmdBlasTranspose transb = (TransB == CblasNoTrans) ? clAmdBlasNoTrans : clAmdBlasTrans;
+  /* Setup OpenCL environment. */
+  err = clGetPlatformIDs(1, &platform, NULL);
+  err = clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, &device, NULL);
+  props[1] = (cl_context_properties)platform;
+  ctx = clCreateContext(props, 1, &device, NULL, NULL, &err);
+  queue = clCreateCommandQueue(ctx, device, 0, &err);
+  /* Setup clAmdBlas. */
+  err = clAmdBlasSetup();
+  if (err != CL_SUCCESS) {
+    printf("clAmdBlasSetup() failed with %d\n", err);
+    clReleaseCommandQueue(queue);
+    clReleaseContext(ctx);
+    return 1;
+  }
+  // Create buffers
+  bufA = clCreateBuffer(ctx, CL_MEM_READ_ONLY,  max_size*max_size*sizeof(double), NULL, &err);
+  bufB = clCreateBuffer(ctx, CL_MEM_READ_ONLY,  max_size*max_size*sizeof(double), NULL, &err);
+  bufC = clCreateBuffer(ctx, CL_MEM_READ_WRITE, max_size*max_size*sizeof(double), NULL, &err);
+  // Dummy
+  M = N = K = lda = ldb = ldc = 512;
+  if (Order == CblasColMajor) {
+    DGEMM_COL();
+  } else {
+    DGEMM_ROW();
+  }
+  err = clFinish(queue);
+#endif
+
   printf("               M    N    K\n");
-  for (int M = stride; M <= max_size; M += stride) {
-    int N, K, lda, ldb, ldc, sizeA, sizeB, sizeC;
+  for (M = stride; M <= max_size; M += stride) {
     N = K = M;
     //K = 128;
-    if (order == CblasColMajor) {
-      lda = (transA == CblasTrans) ? K : M;
-      ldb = (transB == CblasTrans) ? N : K;
+    if (Order == CblasColMajor) {
+      lda = (TransA == CblasTrans) ? K : M;
+      ldb = (TransB == CblasTrans) ? N : K;
       ldc = M;
     } else {
-      lda = (transA == CblasTrans) ? M : K;;
-      ldb = (transB == CblasTrans) ? K : N;
+      lda = (TransA == CblasTrans) ? M : K;;
+      ldb = (TransB == CblasTrans) ? K : N;
       ldc = N;
     }
     sizeA = M*K;
@@ -68,25 +114,37 @@ int main(int argc, char *argv[])
     lapackf77_dlarnv(&ione, ISEED, &sizeC, C);
     memcpy(C2, C, sizeC*sizeof(double));
 
+#ifdef TESTING_AMDCLBLAS
+    err = clEnqueueWriteBuffer(queue, bufA, CL_TRUE, 0, sizeA*sizeof(double), A, 0, NULL, NULL);
+    err = clEnqueueWriteBuffer(queue, bufB, CL_TRUE, 0, sizeB*sizeof(double), B, 0, NULL, NULL);
+    err = clEnqueueWriteBuffer(queue, bufC, CL_TRUE, 0, sizeC*sizeof(double), C, 0, NULL, NULL);
+#endif
+
     printf("dgemm %c%c%c : %4d %4d %4d",
-        (order == CblasColMajor) ? 'C' : 'R',
-        (transA == CblasNoTrans) ? 'N' : 'T',
-        (transB == CblasNoTrans) ? 'N' : 'T',
+        (Order == CblasColMajor) ? 'C' : 'R',
+        (TransA == CblasNoTrans) ? 'N' : 'T',
+        (TransB == CblasNoTrans) ? 'N' : 'T',
         M, N, K);
 
     double flops = FLOPS_DGEMM(M, N, K);
     start = my_get_current_time();
-    if (order == CblasColMajor) {
+    if (Order == CblasColMajor) {
       DGEMM_COL();
     } else {
       DGEMM_ROW();
     }
+#ifdef TESTING_AMDCLBLAS
+    err = clFinish(queue);
+#endif
     end = my_get_current_time();
     comp_time = end - start;
     perf = flops / comp_time / 1e9;
 
     if (error_check) {
-      cblas_dgemm(order, transA, transB, M, N, K, alpha, A, lda, B, ldb, beta, C2, ldc);
+#ifdef TESTING_AMDCLBLAS
+      err = clEnqueueReadBuffer(queue, bufC, CL_TRUE, 0, M*N*sizeof(double), C, 0, NULL, NULL);
+#endif
+      cblas_dgemm(Order, TransA, TransB, M, N, K, alpha, A, lda, B, ldb, beta, C2, ldc);
       blasf77_daxpy(&sizeC, &neg_one, C, &ione, C2, &ione);
       error = lapackf77_dlange("M", &M, &N, C2, &ldc, work);
       printf(" : %10.6lf sec %12.6lf GFlop/s   %e", comp_time, perf, error);
@@ -95,17 +153,28 @@ int main(int argc, char *argv[])
     }
 
     start = my_get_current_time();
-    if (order == CblasColMajor) {
+    if (Order == CblasColMajor) {
       DGEMM_COL();
     } else {
       DGEMM_ROW();
     }
+#ifdef TESTING_AMDCLBLAS
+    err = clFinish(queue);
+#endif
     end = my_get_current_time();
     comp_time = end - start;
     perf = flops / comp_time / 1e9;
     printf(" : %10.6lf sec %12.6lf GFlop/s\n", comp_time, perf);
   }
   free(A); free(B); free(C); free(C2);
+#ifdef TESTING_AMDCLBLAS
+  clReleaseMemObject(bufC);
+  clReleaseMemObject(bufB);
+  clReleaseMemObject(bufA);
+  clReleaseCommandQueue(queue);
+  clReleaseContext(ctx);
+  clAmdBlasTeardown();
+#endif
 
   return 0;
   }
